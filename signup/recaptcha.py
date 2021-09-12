e60ef527aa3c3450e3aac176ec9060548f58a145
Eddie Cosma <36821604+eddie-cosma@users.noreply.github.com>
2021-09-12 16:37:17 -0400

    Email validation

---
+++ signup/recaptcha.py
@@ -0,0 +1,18 @@
+from flask import request, current_app
+import requests
+
+
+def verify_recaptcha(token: str) -> bool:
+    recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify'
+    recaptcha_secret_key = current_app.config['RECAPTCHA_SECRET_KEY']
+    payload = {
+        'secret': recaptcha_secret_key,
+        'response': token,
+        'remoteip': request.remote_addr,
+    }
+    response = requests.post(recaptcha_url, data=payload)
+    if response.status_code == 200:
+        print(response.json())
+        return response.json()['success']
+
+    return False
--- signup/routes.py
+++ signup/routes.py
@@ -1,19 +1,26 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, current_app, abort
 
 from . import db
 from .email import validate
 from .models import User
+from .recaptcha import verify_recaptcha
 
 bp = Blueprint('shortage', __name__)
 
 
 @bp.route('/', methods=['GET', 'POST'])
 def signup():
+    recaptcha_key = current_app.config['RECAPTCHA_SITE_KEY']
     email = request.form.get('email', None)
+
     if request.method == 'POST' and email:
+        recaptcha_token = request.form.get('g-recaptcha-response')
+        if not verify_recaptcha(recaptcha_token):
+            abort(401)
+
         if registrant := db.session.query(User).filter_by(email=email).one_or_none():
             if registrant.opt_in_code:
-                User.send_opt_in_confirmation(email)
+                # User.send_opt_in_confirmation(email)
                 flash('Re-sending confirmation email. Please check your inbox to confirm this registration.')
             else:
                 flash('This email address is already registered.')
@@ -23,11 +30,11 @@ def signup():
             db.session.commit()
 
             User.generate_keys(email)
-            User.send_opt_in_confirmation(email)
+            # User.send_opt_in_confirmation(email)
         else:
             flash('This email address is invalid.')
 
-    return render_template('signup.html', registrant=email)
+    return render_template('signup.html', registrant=email, recaptcha_key=recaptcha_key)
 
 
 @bp.route('/confirm/<token>', methods=['GET'])
